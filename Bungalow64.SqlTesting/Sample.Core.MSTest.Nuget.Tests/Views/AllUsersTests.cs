fbdbf27f6d762c9c6b5a67a5369572d3bb70133b
Jamie Burns <evilbhonda@gmail.com>
2020-10-15 13:53:09 +0100

    Updated author and added new test project to use published package
    Related Work Items: #37

---
+++ Bungalow64.SqlTesting/Sample.Core.MSTest.Nuget.Tests/Views/AllUsersTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using Models.DataResults;
+using System.Threading.Tasks;
+using Frameworks.MSTest2;
+
+namespace Sample.Core.MSTest.Nuget.Tests.Views
+{
+    [TestClass]
+    public class AllUsersTests : TestBase
+    {
+        [TestMethod]
+        public async Task AllUsers_NoData_NothingReturned()
+        {
+            QueryResult results = await TestRunner.ExecuteViewAsync("dbo.AllUsers");
+
+            results
+                .AssertRowCount(0);
+
+            Assert.AreEqual(0, await TestRunner.CountRowsInViewAsync("dbo.AllUsers"));
+        }
+
+        [TestMethod]
+        public async Task AllUsers_OneRow_OneUserReturned()
+        {
+            await TestRunner.ExecuteStoredProcedureNonQueryAsync("dbo.AddUser",
+                new SqlParameter("FirstName", "Jamie"),
+                new SqlParameter("LastName", "Burns"),
+                new SqlParameter("EmailAddress", "jamie@bungalow64.co.uk"),
+                new SqlParameter("StartDate", DateTime.Parse("01-Mar-2020")),
+                new SqlParameter("NumberOfHats", 14),
+                new SqlParameter("Cost", 15.87));
+
+            QueryResult results = await TestRunner.ExecuteViewAsync("dbo.AllUsers");
+
+            results
+                .AssertRowCount(1)
+                .AssertRowValues(0, new DataSetRow
+                {
+                    { "FirstName", "Jamie" }
+                });
+
+            Assert.AreEqual(1, await TestRunner.CountRowsInViewAsync("dbo.AllUsers"));
+        }
+
+        [TestMethod]
+        public async Task AllUsers_TwoRows_TwoUsersReturned()
+        {
+            await TestRunner.ExecuteStoredProcedureNonQueryAsync("dbo.AddUser",
+                new SqlParameter("FirstName", "Jamie"),
+                new SqlParameter("LastName", "Burns"),
+                new SqlParameter("EmailAddress", "jamie@bungalow64.co.uk"),
+                new SqlParameter("StartDate", DateTime.Parse("01-Mar-2020")),
+                new SqlParameter("NumberOfHats", 14),
+                new SqlParameter("Cost", 15.87));
+
+            await TestRunner.ExecuteStoredProcedureNonQueryAsync("dbo.AddUser",
+                new SqlParameter("FirstName", "Stuart"),
+                new SqlParameter("LastName", "Burns"),
+                new SqlParameter("EmailAddress", "stuart@bungalow64.co.uk"),
+                new SqlParameter("StartDate", DateTime.Parse("01-Mar-2020")),
+                new SqlParameter("NumberOfHats", 14),
+                new SqlParameter("Cost", 15.87));
+
+            QueryResult results = await TestRunner.ExecuteViewAsync("dbo.AllUsers");
+
+            results
+                .AssertRowCount(2)
+                .AssertRowExists(new DataSetRow
+                {
+                    { "FirstName", "Jamie" }
+                })
+                .AssertRowExists(new DataSetRow
+                {
+                    { "FirstName", "Stuart" }
+                });
+        }
+    }
+}
