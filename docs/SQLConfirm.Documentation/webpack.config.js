8e3efdd7d289ef6a30a95f2302ded7b22e5afde5
Jamie Burns <evilbhonda@gmail.com>
2020-10-27 14:38:29 +0000

    Added start of documentation
    Related Work Items: #78

---
+++ docs/SQLConfirm.Documentation/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'favicon.ico', to: '' },
+                { from: 'src/index.html', to: '' },
+                { from: 'src/styles', to: 'styles' }
+            ]
+        })
+    ]
+};
