e49fea0cc3847eb112022a13c3e15102f22d6ac3
Jamie Burns <evilbhonda@gmail.com>
2020-10-29 11:31:12 +0000

    Updated styles
    Related Work Items: #78

--- docs/DBConfirm.Documentation/dbconfirm.documentation/src/App.js
+++ docs/DBConfirm.Documentation/dbconfirm.documentation/src/App.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
+import React, { useRef } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 import Overview from './pages/Overview';
 import QuickStart from './pages/QuickStart';
 import NuGet from './pages/NuGet';
 import Templates from './pages/Templates';
 import ContinuousIntegration from './pages/ContinuousIntegration';
 import Api from './pages/Api'
-import ScrollToTop from './ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 import './App.scss';
 
 function App() {
+
+  let contentRef = useRef();
+
   return (
     <Router>
-      <div id="sidebar">
-        <div id="sidebar-heading">
-          DBConfirm
-        </div>
-        <Menu/>
-        <Footer/>
-      </div>
-      <div id="content">
-        <ScrollToTop/>
-          <h1>DBConfirm - Documentation</h1>
+      <Sidebar/>
+      <div id="content" ref={contentRef}>
+        <ScrollToTop scrollContent={contentRef}/>
+          <h1>DBConfirm - Official Documentation</h1>
           <Switch>
             <Route path="/quickstart"><QuickStart /></Route>
             <Route path="/nuget"><NuGet /></Route>
--- docs/DBConfirm.Documentation/dbconfirm.documentation/src/components/ScrollToTop.js
+++ docs/DBConfirm.Documentation/dbconfirm.documentation/src/components/ScrollToTop.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export default function ScrollToTop() {
+export default function ScrollToTop({scrollContent}) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    scrollContent.current.scrollTop = 0;
   }, [pathname]);
 
   return null;
