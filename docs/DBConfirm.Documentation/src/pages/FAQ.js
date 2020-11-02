bd21316ee4ade37c4df74d873bce3c9a1431f9b3
Jamie Burns <evilbhonda@gmail.com>
2020-11-02 12:35:44 +0000

    Added FAQs
    Related Work Items: #40

---
+++ docs/DBConfirm.Documentation/src/pages/FAQ.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function FAQ() {
+    return (
+        <>
+            <h2>Frequently Asked Questions</h2>
+            <h3>Are there any limitations on what kind of project I can use DBConfirm in?</h3>
+            <p>No.  Go for it.</p>
+            <h3>Is DBConfirm free?</h3>
+            <p>Yes.</p>
+            <h3>Can I run the tests in parallel?</h3>
+            <p>No.  Due to how the data in the database is used, it would be very easy to introduce deadlocks if the tests are run in parallel, so being able to run the tests in parallel has been disabled.</p>
+            <h3>Do I need an instance of the database to test against?</h3>
+            <p>Yes.  DBConfirm doesn't handle the deployment of the latest version of your database, it just requires configuration to point to the database to test.</p>
+            <h3>Is this white-box, black-box or gray-box testing?</h3>
+            <p>Gray-box testing.  You need to know how the tables in the database are structured, but you don't need to know how the rest of the database is put together.</p>
+            <h3>Can I use data-driven tests?</h3>
+            <p>Yes.  DBConfirm works really well with <code>[DataTestMethod]</code> (MSTest) and <code>[TestCase]</code> (NUnit).</p>
+        </>
+    );
+}
