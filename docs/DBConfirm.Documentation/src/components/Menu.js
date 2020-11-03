21064d7f2f76e490e32ff804d2fca5691f19c593
Jamie Burns <evilbhonda@gmail.com>
2020-11-03 15:41:01 +0000

    Added docs on connection strings, and made it possible to open a connection using a connection string name (instead of just value).
    Related Work Items: #94

--- docs/DBConfirm.Documentation/src/components/Menu.js
+++ docs/DBConfirm.Documentation/src/components/Menu.js
@@ -10,6 +10,7 @@ export default function Menu() {
                 <li><NavLink to="/writingtests" activeClassName="active">Writing Tests</NavLink></li>
                 <li><NavLink to="/templates" activeClassName="active">Templates</NavLink></li>
                 <li><NavLink to="/nuget" activeClassName="active">NuGet Packages</NavLink></li>
+                <li><NavLink to="/connectionstrings" activeClassName="active">Connection Strings</NavLink></li>
                 <li><NavLink to="/continuousintegration" activeClassName="active">Continuous Integration</NavLink></li>
                 <li><NavLink to="/debugging" activeClassName="active">Debugging</NavLink></li>
                 <li><NavLink to="/api" activeClassName="active">API Reference</NavLink></li>
--- docs/DBConfirm.Documentation/src/pages/ContinuousIntegration.js
+++ docs/DBConfirm.Documentation/src/pages/ContinuousIntegration.js
@@ -13,18 +13,20 @@ export default function ContinuousIntegration(){
             so this database needs to be accessible by the build agent.</p>
         <p>There are 3 ways to configure the connection string used by the tests, which are checked in this order:
         </p>
-        <ul>
-            <li><a href="#environmentvariables">1. Environment Variables</a></li>
-            <li><a href="#runsettings">2. .runsettings</a></li>
-            <li><a href="#appsettings">3. appsettings.json</a></li>
-        </ul>
+        <ol>
+            <li><a href="#environmentvariables">Environment Variables</a></li>
+            <li><a href="#runsettings">.runsettings</a></li>
+            <li><a href="#appsettings">appsettings.json</a></li>
+        </ol>
         <h3 id="environmentvariables">Configure using Environment Variables</h3>
         <p>This is the recommended approach to configuring the CI build.</p>
-        <p>During the test initialisation, the current environment variables are checked for a variable called
+        <p>During the test initialisation, by default, the current environment variables are checked for a variable called
             '<strong>DefaultConnectionString</strong>', and if
             a value is found, this connection string is used.</p>
-        <p>To set this environment variable, see the configuration for the CI build itself.</p>
-        <p>For example, in Azure DevOps, the environment variables are listed as '<strong>Variables</strong>':
+            <p>If different connection string names have been set in the test project, then variables should exist for those too.  For more details on setting
+                the connection string names and values, see <a href="/connectionstrings">Connection Strings</a>.
+            </p>
+        <p>To set this environment variable, see the configuration for the CI build itself.  For example, in Azure DevOps, the environment variables are listed as '<strong>Variables</strong>':
         </p>
         <p><img src={ci_connectionstring}
                 alt="The ConnectionString environment variable added to a Pipeline build in Azure DevOps"
@@ -34,7 +36,7 @@ export default function ContinuousIntegration(){
         <h3 id="runsettings">Configure using a .runsettings file</h3>
         <p>During test initialisation, if no environment variable is found, DBConfirm will next check for a
             .runsettings file.</p>
-        <p>If a .runsettings file is found, a parameter called '<strong>DefaultConnectionString</strong>' is checked, and used
+        <p>If a .runsettings file is found, a parameter called '<strong>DefaultConnectionString</strong>' (or the customised connection string name) is checked, and used
             if found.</p>
         <p>An example .runsettings file is:</p>
         <pre><code className="lang-xml"><span className="hljs-meta">&lt;?</span><span className="hljs-tag">xml</span> <span className="hljs-attr">version</span>="<span className="hljs-string">1.0</span>" <span className="hljs-attr">encoding</span>="<span className="hljs-string">utf-8</span>"<span className="hljs-meta">?&gt;</span>
@@ -48,7 +50,7 @@ export default function ContinuousIntegration(){
         <h3 id="appsettings">Configure using appsettings.json</h3>
         <p>This method is recommended for local development and testing, but since it can't easily be changed as part of a CI build, it should only be used as a last resort for builds.</p>
         <p>During test initialisation, if no environment variable or .runsettings variable is found, then the appsettings.json file is checked for a connection string 
-            with a name of '<strong>DefaultConnectionString</strong>'.
+            with a name of '<strong>DefaultConnectionString</strong>' (or the customised connection string name).
         </p>
         <p>An example appsettings.json file is:</p>
         <pre><code className="lang-json">{"{"}
